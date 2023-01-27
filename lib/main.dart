8410d346ee36fb90fc2185a3fede859095e8302a
ReachPooja <reachpoojaprajapat@gmail.com>
2023-01-27 22:55:47 +0530

    chore: bootstrap

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:water_tracker/app/app.dart';
+import 'package:water_tracker/app/bootstrap.dart';
+import 'package:water_tracker/app/injector/injector.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  configureInjector();
+
+  await Hive.initFlutter();
+
+  await bootstrap(MyApp.new);
 }
