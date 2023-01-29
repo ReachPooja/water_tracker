a135019469a808d5ddf6385b4df9cd6f3605922c
ReachPooja <reachpoojaprajapat@gmail.com>
2023-01-29 22:51:23 +0530

    feat: user adapter

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:water_tracker/app/app.dart';
 import 'package:water_tracker/app/bootstrap.dart';
 import 'package:water_tracker/app/injector/injector.dart';
+import 'package:water_tracker/src/user/model/user.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,6 +11,7 @@ Future<void> main() async {
   configureInjector();
 
   await Hive.initFlutter();
+  Hive.registerAdapter(UserAdapter());
 
   await bootstrap(MyApp.new);
 }
