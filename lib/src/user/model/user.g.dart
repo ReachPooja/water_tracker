5ef831020fda6c662f29544bcb56a6e9dd44dba3
ReachPooja <reachpoojaprajapat@gmail.com>
2023-01-29 20:16:13 +0530

    feat: user model

---
+++ lib/src/user/model/user.g.dart
@@ -0,0 +1,50 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class UserAdapter extends TypeAdapter<User> {
+  @override
+  final int typeId = 1;
+
+  @override
+  User read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return User(
+      gender: fields[0] as String,
+      weight: fields[1] as int,
+      wakeUpTime: fields[2] as int,
+      bedTime: fields[3] as int,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, User obj) {
+    writer
+      ..writeByte(4)
+      ..writeByte(0)
+      ..write(obj.gender)
+      ..writeByte(1)
+      ..write(obj.weight)
+      ..writeByte(2)
+      ..write(obj.wakeUpTime)
+      ..writeByte(3)
+      ..write(obj.bedTime);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is UserAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
